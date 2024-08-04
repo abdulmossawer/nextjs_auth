0ec5f2ea74d788f90141b2caf029ed8b0ba79ac9
abdulmossawer <mossawer786@gmail.com>
2024-08-05 01:16:56 +0530

    Complete Signup Route and Mailer

---
+++ src/models/userModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    username : {
+        type: String,
+        required: [true , 'Please provide a ussername'],
+        unique: [true, 'Please enter unique username'],
+        trim:[true, "Don't use any spaces"]
+    },
+
+    email : {
+        type: String,
+        required: [true , 'Please provide a ussername'],
+        unique: [true, 'Please enter unique username'],
+        trim: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email address']
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [12, 'Password must be at least 12 characters long'],
+        maxlength: [64, 'Password must be at most 64 characters long'],
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+    forgotPasswordToken : String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry : Date
+
+})
+
+const User = mongoose.model.users || mongoose.model("users", userSchema)
+
+export default User;
+
