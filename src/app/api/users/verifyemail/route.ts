8bab6976eb7f8b00cfbcf95e99fc6dcf6b353edf
abdulmossawer <mossawer786@gmail.com>
2024-08-05 15:56:23 +0530

    error fixed

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,37 +1,39 @@
 import {connect} from "@/dbConfig/dbConfig";
-import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+
 
 
 connect()
 
-export async function POST(request: NextRequest) {
+
+export async function POST(request: NextRequest){
+
     try {
         const reqBody = await request.json()
         const {token} = reqBody
-
         console.log(token);
-        
-        //Find user via token
-        const user =  await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}})
 
-        //check if user does't exists
+        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
+
         if (!user) {
-           return NextResponse.json({message: 'Invalid Token'}, {status: 400})
+            return NextResponse.json({error: "Invalid token"}, {status: 400})
         }
+        console.log(user);
 
-        user.isVerified = true,
-        user.verifyToken = undefined,
-        user.verifyTokenExpiry = undefined
-        await user.save()
-
+        user.isVerfied = true;
+        user.verifyToken = undefined;
+        user.verifyTokenExpiry = undefined;
+        await user.save();
+        
         return NextResponse.json({
             message: "Email verified successfully",
             success: true
         })
 
-    }  catch (error:any) {
+
+    } catch (error:any) {
         return NextResponse.json({error: error.message}, {status: 500})
     }
-    
+
 }
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -5,18 +5,14 @@ import User from "@/models/userModel";
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
     // create a hased token
-    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
     if (emailType === "VERIFY") {
-      await User.findByIdAndUpdate(userId, {
-        verifyToken: hashedToken,
-        verifyTokenExpiry: Date.now() + 3600000,
-      });
+      await User.findByIdAndUpdate(userId,
+        { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 })
     } else if (emailType === "RESET") {
-      await User.findByIdAndUpdate(userId, {
-        forgotPasswordToken: hashedToken,
-        forgotPasswordTokenExpiry: Date.now() + 3600000,
-      });
+      await User.findByIdAndUpdate(userId,
+        { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 })
     }
 
     const transport = nodemailer.createTransport({
@@ -29,23 +25,21 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
     });
 
     const mailOptions = {
-      from: "hitesh@gmail.com",
+      from: "sadiq@gmail.com",
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: `<p>Click <a href="${
-        process.env.DOMAIN
-      }/verifyemail?token=${hashedToken}">here</a> to ${
-        emailType === "VERIFY" ? "verify your email" : "reset your password"
-      }
-          or copy and paste the link below in your browser. <br> ${
-            process.env.DOMAIN
-          }/verifyemail?token=${hashedToken}
+      html: `<p>Click <a href="${process.env.DOMAIN
+        }/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"
+        }
+          or copy and paste the link below in your browser. <br> ${process.env.DOMAIN
+        }/verifyemail?token=${hashedToken}
           </p>`,
     };
 
-    const mailResponse = await transport.sendMail(mailOptions);
-    return mailResponse;
+    const mailresponse = await transport.sendMail
+      (mailOptions);
+    return mailresponse;
   } catch (error: any) {
     throw new Error(error.message);
   }
